fff8e114d91dab81aad7498572d840e4cf667473
SalomeCx <salome.coavoux@gmail.com>
2012-12-03 19:03:37 +0100

    ecriture normale, sauf l'UTF8

--- options.c
+++ options.c
@@ -13,10 +13,10 @@
 #include <time.h>
 #include "options.h"
 
+// Créer l'archive, et les entêtes.
+char stime2[sizeof "JJ/MM/AAAA HH:MM:SS"]; //[sizeof("JJ/MM/AAAA HH:MM:SS")];
 
-char stime2[sizeof "JJ/MM/AAAA HH:MM:SS"];//[sizeof("JJ/MM/AAAA HH:MM:SS")];
-
-int creer_archive(char* nomArchive, char* tabFichiers[], int nbFiles){
+void creer(char* nomArchive, char* tabFichiers[], int nbFiles){
   // Fichier archive.
   FILE * archive;
 
@@ -24,67 +24,79 @@ int creer_archive(char* nomArchive, char* tabFichiers[], int nbFiles){
   int fichier;
   char * tmp = malloc(sizeof(char));
 
-  //char buff;
-
   archive = fopen(nomArchive, "w+");
-  for(int i = 0; i<nbFiles; i++){ 
-    fichier = open(tabFichiers[i],O_RDONLY, S_IRUSR | S_IWUSR);
-
-    int taille = tailleFichier(tabFichiers[i]);
-    int permission = permissions(tabFichiers[i]);
-    modification(tabFichiers[i]);
-    
-    // On écrit la taille du fichier.
-    tmp = realloc(tmp, sizeof(char)*sizeof(taille));
-    sprintf(tmp, "%d", taille);
-    fwrite(tmp, 1, sizeof(tmp), archive);
-    fwrite(" ", 1, 1, archive);
-    
-    // Les permissions du fichier.
-    tmp = realloc(tmp, sizeof(char)*sizeof(permission));
-    sprintf(tmp, "%d", permission);
-    fwrite(tmp, 1, sizeof(tmp), archive);
-    fwrite(" ", 1, 1, archive);
-
-    fwrite(stime2, 1, sizeof(stime2), archive);
-    fwrite(" ", 1, 1, archive);
-
-    //lseek(fichier, SEEK_SET, 1);
-    /*while((read(fichier, &buff, 0)) > 0){
-      printf("test %s\n", buff);
-      fwrite(&buff, sizeof(char), taille, archive);
-      }*/
-    close(fichier);
+  for(int i = 0; i<nbFiles; i++)
+    { 
+      fichier = open(tabFichiers[i], O_RDONLY, S_IRUSR | S_IWUSR);
+      
+      int taille = tailleFichier(tabFichiers[i]);
+      int permission = permissions(tabFichiers[i]);
+      //modification(tabFichiers[i]);
+      
+      fwrite(tabFichiers[i], 1, sizeof(tabFichiers[i]), archive);
+      
+      // On écrit la taille du fichier.
+      tmp = realloc(tmp, sizeof(char)*sizeof(taille));
+      sprintf(tmp, "%d", taille);
+      fwrite(tmp, 1, sizeof(tmp), archive);
+      //fwrite(" ", 1, 1, archive);
+      
+      // Les permissions du fichier.
+      tmp = realloc(tmp, sizeof(char)*sizeof(permission));
+      sprintf(tmp, "%d", permission);
+      fwrite(tmp, 1, sizeof(tmp), archive);
+      //fwrite(" ", 1, 1, archive);
+      
+      // La date de dernière modification
+      fwrite(stime2, 1, sizeof(stime2), archive);
+      //fwrite(" ", 1, 1, archive);
+      // Pour séparer l'entête du reste.
+      fputc('\n', archive);
+      
+      char * buff = malloc(sizeof(char) * taille);
+      lseek(fichier, 0, SEEK_SET);
+      read(fichier, buff, sizeof(char)*taille);
+      fwrite(buff, sizeof(char), taille, archive);
+      
+      close(fichier);
+      free(buff);
   }
   free(tmp);
   fclose(archive);
 }
 
 /* Retourne la taille du fichier sous forme d'un entier. */
-int tailleFichier(char* fichier){
+int tailleFichier(char* fichier)
+{
   int fd = open(fichier, O_RDONLY);
   off_t s = lseek(fd, 0, SEEK_END);
-
+  
   close(fd);
   return s;
-  }
+}
 
-int permissions(char* fichier){
+/* retourne les permissions sur le fichier. */
+int permissions(char* fichier)
+{
   struct stat permission;
   if (stat(fichier, &permission) == -1)
     exit(1);
   return (permission.st_mode & 0777);
 }
 
-void modification(char* fichier){
+/* Retourne la date de dernière modification. */
+void modification(char* fichier)
+{
   struct stat permission;
   if (stat(fichier, &permission) == -1)
     exit(1);
-  //char stime[sizeof "JJ/MM/AAAA HH:MM:SS"];
+  
   struct tm time = *localtime(&permission.st_mtime);
   strftime (stime2, sizeof stime2, "%d/%m/%Y %H:%M:%S", &time);
-  //printf("%d\n", sizeof(stime));
-  //printf("%s\n", stime);
+}
+
+void list(void)
+{
 }
 
 int main(int argc, char* argv[]){
@@ -97,7 +109,7 @@ int main(int argc, char* argv[]){
       tab[i] = argv[i + 1];
     }
 
-  creer_archive(nom, tab, argc - 1);
-  modification(argv[1]);
-  }
+  creer(nom, tab, argc - 1);
+  //  modification(argv[1]);
+}
   
