beba00bf3d8dc7836cad79be786a127febf3e71f
SalomeCx <salome.coavoux@gmail.com>
2012-12-08 16:45:57 +0100

    commentaires

--- lire.c
+++ lire.c
@@ -14,7 +14,7 @@
 
 #include "lire.h"
 
-// Renvoie 0 si la lecture s'est bien passée, -1 si l'ouverture de l'archive s'est mal passée, et 1 si la lecture s'est mal passée.
+/* Renvoie 0 si la lecture s'est bien passée, -1 si l'ouverture de l'archive s'est mal passée, et 1 si la lecture s'est mal passée.*/
 int lireEntetes(char * nomArchive, Fichier * listHd, int compteur){
   FILE * archive = fopen(nomArchive, "r");
   if (archive == NULL)
@@ -33,11 +33,11 @@ int lireEntetes(char * nomArchive, Fichier * listHd, int compteur){
 
   fclose(archive);
   return 0;
-  // Il faudra penser à free les headers dans le main.
+  /* Il faudra penser à libérer la mémoire des headers dans la fonction appelante. */
 }
 
-// Duplication de code. Pas d'autre idées pour récupérer le nombre de fichiers dans l'archive.
-// Passer un int * en paramètre à lireEntetes ne fonctionne pas (warnings et memory map error).
+/* Duplication de code. Pas d'autre idées pour récupérer le nombre de fichiers dans l'archive.
+   Passer un int * en paramètre à lireEntetes ne fonctionne pas (warnings et memory map error). */
 int nbEntetes(char * nomArchive)
 {
   FILE * archive = fopen(nomArchive, "r");
@@ -51,7 +51,7 @@ int nbEntetes(char * nomArchive)
 
       fread(hder, sizeof(struct fichier), 1, archive);
 
-      //Quand on arrive à un fichier de taille 0, c'est-à-dire quand on a fini la lecture.
+      /* Quand on arrive à un fichier de taille 0, c'est-à-dire quand on a fini la lecture. */
       if (hder->taille == 0)
 	break;
 
@@ -64,8 +64,8 @@ int nbEntetes(char * nomArchive)
   return cpt;
 }
 
-// Retourne la position du fichier file dans l'archive nomArchive.
-// Duplication de code avec rmFile (dans options.c).
+/* Retourne la position du fichier file dans l'archive nomArchive.
+   Duplication de code avec rmFile (dans options.c). */
 long chercherFichier(char * nomArchive, char * file)
 {
   long position = 0;
@@ -79,7 +79,7 @@ long chercherFichier(char * nomArchive, char * file)
 
   for (int i = 0; i < nb; i++)
     {
-      // Une fois que l'on a trouvé le fichier correspondant dans l'archive.
+      /* Une fois que l'on a trouvé le fichier correspondant dans l'archive. */
       if (strcmp(file, headers[i]->nom) == 0)
 	{
 	  fclose(archive);
@@ -87,7 +87,7 @@ long chercherFichier(char * nomArchive, char * file)
 	}
       else
 	{
-	  // Tant qu'on ne trouve pas le fichier, on avance de la taille du fichier courant + la taille de son header.
+	  /* Tant qu'on ne trouve pas le fichier, on avance de la taille du fichier courant + la taille de son header. */
 	  position += sizeof(struct fichier);
 	  position += headers[i]->taille;
 	}
